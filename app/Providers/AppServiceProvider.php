c256ff73edcb8a11af383146dee17e72d6e0bc7b
Meng Lang <kcaribou0428@gmail.com>
2020-11-24 22:40:01 +1000

    setup default string length

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Default length of string.
+        \Illuminate\Support\Facades\Schema::defaultStringLength(191);
     }
 }
