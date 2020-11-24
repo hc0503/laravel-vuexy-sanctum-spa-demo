eede2fc6c3b96b10be838e23f18b05a8dec519f4
Meng Lang <kcaribou0428@gmail.com>
2020-11-24 22:40:38 +1000

    api routes

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/login', 'API\Auth\AuthController@login')->name('login');
+
+Route::group(['prefix' => 'users'], function () {
+    Route::get('/', 'API\UserController@getList');
+    Route::post('create', 'API\UserController@postCreate');
+    Route::post('update/{user}', 'API\UserController@postUpdate');
+    Route::post('delete/{user}', 'API\UserController@postDelete');
+});
+Route::group(['prefix' => 'roles'], function () {
+    Route::get('/', 'API\RoleController@getList');
+    Route::post('create', 'API\RoleController@postCreate');
+    Route::post('update/{role}', 'API\RoleController@postUpdate');
+    Route::post('delete/{role}', 'API\RoleController@postDelete');
+});
+
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('logout', 'API\Auth\AuthController@getLogout');
 });
