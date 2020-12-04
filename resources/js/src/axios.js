cb3c206bcb4cc6f027d4d75c0424ae04d48dabf1
Meng Lang <kcaribou0428@gmail.com>
2020-12-04 17:37:59 +1000

    When unauthorize, go to login

--- resources/js/src/axios.js
+++ resources/js/src/axios.js
@@ -1,5 +1,6 @@
 // axios
 import axios from 'axios'
+import router from '@/router'
 
 const baseURL = 'http://localhost:8000'
 const withCredentials = true
@@ -8,5 +9,15 @@ const instance = axios.create({
   baseURL,
   withCredentials
 });
+
+instance.interceptors.response.use(
+  response => {
+      return response;
+  },
+  error => {
+      if (error.response.status === 401) {
+          router.push({ name: 'page-login' })
+      }
+  })
 
 export default instance;
